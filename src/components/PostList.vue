73c574219b2395a77844c6f6ee3252795b64ce5e
golfstrimmar <64115098+golfstrimmar@users.noreply.github.com>
2022-08-28 13:40:07 +0200

    +animation

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,29 +1,35 @@
 <template>
   <!-- v-show='posts.length > 0' -->
-  <div class="posts" v-for='post in posts' :key="post.id" v-if="posts.length > 0">
-    <PostItem :post="post" @remove="$emit('remove', post)"></PostItem>
+  <div class="posts" v-if="posts.length > 0">
+    <h2>posts list</h2>
+    <transition-group name="posts-list">
+      <PostItem
+        v-for="post in posts"
+        :key="post.id"
+        :post="post"
+        @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </div>
+
   <!-- v-show='posts.length === 0' -->
-  <h3 v-else>
-    sory, you have not any posts
-  </h3>
+  <h3 v-else>sory, you have not any posts</h3>
 </template>
 
 <script>
-
-import PostItem from './PostItem'
+import PostItem from "./PostItem";
 
 export default {
   components: {
-    PostItem
+    PostItem,
   },
   props: {
     posts: {
       type: Array,
-      required: true
-    }
-  }
-}
+      required: true,
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -32,7 +38,6 @@ export default {
   display: flex;
   flex-direction: column;
   row-gap: 10px;
-
 }
 
 h3 {
@@ -43,5 +48,23 @@ h3 {
   background: rgb(252, 193, 137);
   border-radius: 5px;
   border: 1px solid red;
+}
+.posts-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+
+.posts-list-enter-active,
+.posts-list-leave-active {
+  transition: all 0.3s ease;
+}
+
+.posts-list-enter-from,
+.posts-list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+.posts-list-move {
+  transition: transform 0.4s ease;
 }
 </style>
